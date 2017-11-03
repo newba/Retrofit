a5b6a439eda371e34d9ae88dc697e265ad297399
Marcelo Santos <mmsprogramador@gmail.com>
2017-11-03 15:04:47 -0400

    Add Comments

---
+++ app/src/main/java/br/com/darksite/tp2mobileavecretrofit/SupplierList.java
@@ -0,0 +1,13 @@
+package br.com.darksite.tp2mobileavecretrofit;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class SupplierList extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.supplier_list);
+    }
+}
